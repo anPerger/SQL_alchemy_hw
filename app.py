085ee3e88748179545ea3fae3e01882baa6a93e2
krazy-kudu <66883099+krazy-kudu@users.noreply.github.com>
2020-08-28 10:21:12 -0700

    websighting

--- app.py
+++ app.py
@@ -1,28 +1,62 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurements = Base.classes.measurement
+Stations = Base.classes.station
+
+app = Flask(__name__)
+
 @app.route("/")
 def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"<a href='/api/v1.0/</a><br/>"
-        f"<a href='/api/v1.0/</a>"
+        f"<a href='/api/v1.0/precipitation'>precipitation</a><br/>"
+        f"<a href='/api/v1.0/stations'>stations</a><br/>"
+        f"<a href='/api/v1.0/tobs'>tobs</a>"
     )
 
 
-@app.route("/api/v1.0/names")
-def names():
+@app.route("/api/v1.0/precipitation")
+def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of all passenger names"""
     # Query all passengers
-    results = session.query(Passenger.name).all()
+    measurement_results = session.query(Measurements.station, Measurements.date,
+        Measurements.prcp, Measurements.tobs).all()
 
+    
+    for each result in measurement_results
+
+    measurement_satation_id = [result[0] for result in measurement_results[:]]
+    measurement_date = [result[1] for result in measurement_results[:]]
+    prcp = [result[2] for result in measurement_results[:]]
+    tobs = [result[3] for result in measurement_results[:]]
+
+    date_prcp = {f'date{measurement_date}' : prcp}
     session.close()
-    print(results)
+    print(date_prcp)
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    date_prcps = list(np.ravel(date_prcp))
 
-    return jsonify(all_names)
+    return jsonify(date_prcps)
 
 
 @app.route("/api/v1.0/passengers")
